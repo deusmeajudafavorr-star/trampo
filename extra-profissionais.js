a8d4c10654a71f08ec063fe91b7c067fcd25c94b
deusmeajudafavorr-star <deusmeajudafavorr@gmail.com>
2026-09-16 20:15:58 -0300

    Adicionar 100 novos prestadores ao TRAMPO

---
+++ extra-profissionais.js
@@ -0,0 +1,108 @@
+const extraProfessionals=`Grandrive Tecnologia do Brasil|Servicos profissionais|(88) 9919-0049|Rua Joaquim Figueiredo, 104 - Triangulo|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+A. F. Acessorios|Servicos profissionais|(88) 3512-1384|Avenida Ailton Gomes, 1375 - Piraja|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Ribeiro Moveis|Marceneiro e moveis|(88) 3511-8870|Rua Leao XIII, 922 - Salesianos|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Natan Lisboa|Servicos profissionais|(88) 9369-8081|Sitio Jurema, 95 - Junco|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Dlp Planejados|Servicos profissionais|(88) 9927-3692|Rua Monsenhor Azarias Sobreira, 851 - Tiradentes|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Wi Moveis|Marceneiro e moveis|(88) 9776-9356|Rua Sao Pedro, 1644 - Salesianos|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Mobiari Planejados|Servicos profissionais|(88) 9613-8460|Avenida Radialista Coelho Alves, 911 - Tiradentes|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Cr Moveis Planejados|Marceneiro e moveis|(88) 9929-6614|Rua Padre Joao Moretti, 476 - Jardim Gonzaga|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Inovart Moveis Projetados|Marceneiro e moveis|(88) 9963-0027|Rua Esmeraldo Cardoso Lobo, 139 - Jose Geraldo da Cruz|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+JV Moveis Planejados|Marceneiro e moveis|(88) 99257-3996|Rua Fca. Pereira de Melo, 45 - Frei Damiao|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Filipe Amorim - Moveis Planejados|Marceneiro e moveis|(88) 99905-7767|Avenida Dr. Yone Rodrigues, 537 - Lagoa Seca|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Simples Moveis Planejados|Marceneiro e moveis|(88) 99999-9008|Rua Francisco Martins de Souza, 641 - Frei Damiao|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+ALYSON MOVEIS|Marceneiro e moveis|(88) 98843-2674|Avenida Ailton Gomes, 2284 - Piraja|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Marcenaria Seu Jonas Moveis Planejados|Marceneiro e moveis|(88) 98869-4724|Avenida Cel. Humberto Bezerra, 552 - Limoeiro|https://images.unsplash.com/photo-1504148455328-c376907d081c?auto=format&fit=crop&w=500&q=80
+Gesso Amanhecer Drywall|Gesso e drywall|(88) 8818-2635|Rua Antonio Adail da Nobrega, 191 - Triangulo|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Imperial Novo Gesso|Gesso e drywall|(88) 9812-1122|Rua Pedro Cardoso Sobreira, 99 - Jardim Gonzaga|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Gesso Modesto|Gesso e drywall|(88) 3571-7520|Avenida Ailton Gomes de Alencar, 1415 - Piraja|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Smart Gesso & Cia.|Gesso e drywall|(88) 3512-3392|Rua Jose Andrade de Lavor, 2412 - Triangulo|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Oficina do Gesso|Gesso e drywall|(88) 8808-0320|Rua Josias Inojosa de Oliveira, 5135 - Distrito Industrial|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+GESSO 3D NOVO JUAZEIRO|Gesso e drywall|(88) 99983-2831|Novo Juazeiro|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Fort Drywall e Steel Frame|Gesso e drywall|(88) 98833-2133|Avenida Padre Cicero, 9726 - Muriti|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+TREVO Drywall|Gesso e drywall|(88) 3571-6019|Rua Josias Inojosa de Oliveira, 5000 - Distrito Industrial|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Drywall fortece|Gesso e drywall|(88) 99656-9586|Rua Boa Vista - Centro|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Jeferson Metalurgica|Serralheria e metalurgica|(88) 9628-7737|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Jj Metalurgica|Serralheria e metalurgica|(88) 3511-4340|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Metalurgica Espoleta|Serralheria e metalurgica|(88) 9847-5175|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Metalurgica Pinheiro|Serralheria e metalurgica|(88) 3587-2340|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Metalurgica e Marmoraria Borges|Serralheria e metalurgica||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Metalurgica Jb Industria e Comercio Ltda|Serralheria e metalurgica||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Jb Industria|Servicos profissionais|(88) 9674-6913|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Kariri Tendas e Locacoes|Serralheria e metalurgica|(88) 3511-4012|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Art Metais|Serralheria e metalurgica|(88) 8817-4469|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Cariri Perfis|Serralheria e metalurgica|(88) 9799-5261|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Metalurgica Alianca|Serralheria e metalurgica|(88) 99704-0035|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+JJ Metalurgica|Serralheria e metalurgica|(88) 98832-6912|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?auto=format&fit=crop&w=500&q=80
+Bandeira Guincho|Guincho e socorro mecanico|(88) 9695-9745|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Osmar Guincho|Guincho e socorro mecanico|(88) 2156-3019|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Sb Taxi Ltda|Guincho e socorro mecanico|(85) 3444-9969|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Central Metropolitana Radio Taxi|Guincho e socorro mecanico|(88) 8844-9814|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Cotarc Ltda|Servicos profissionais|(88) 8117-2030|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Sao Jose Servicos|Guincho e socorro mecanico|(88) 9604-6660|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Autotatica 24 Horas|Guincho e socorro mecanico|(88) 4101-2031|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Resgate Assistencia|Guincho e socorro mecanico|(88) 3512-1105|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Eduguincho|Guincho e socorro mecanico|(88) 2156-3019|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Dedetizadora Matamil|Dedetizacao e controle de pragas|(88) 8821-2974|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+Insect Dedetizadora|Dedetizacao e controle de pragas|(88) 8132-2707|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+Ddc - Desintupidora e Dedetizadora Confianca|Dedetizacao e controle de pragas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+Jrt Servicos|Servicos profissionais|(88) 8872-7449|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+G & G Controle de Pragas|Dedetizacao e controle de pragas|(88) 3571-5803|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+Je Climatizacao e Refrigeracao|Ar condicionado e refrigeracao|(88) 8821-0171|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1631545806609-4e4f6e9e1f8b?auto=format&fit=crop&w=500&q=80
+Tec-maqui|Servicos profissionais|(88) 9714-3698|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Caririsamp Servicos|Servicos automotivos|(88) 9914-4936|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1486006920555-c77dcf18193c?auto=format&fit=crop&w=500&q=80
+Kariri Ar|Ar condicionado e refrigeracao|(85) 9615-4066|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1631545806609-4e4f6e9e1f8b?auto=format&fit=crop&w=500&q=80
+Af Refrigeracao|Ar condicionado e refrigeracao|(88) 8107-5651|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1631545806609-4e4f6e9e1f8b?auto=format&fit=crop&w=500&q=80
+Protech Refrigeracao|Ar condicionado e refrigeracao||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1631545806609-4e4f6e9e1f8b?auto=format&fit=crop&w=500&q=80
+Casas Kariri|Servicos profissionais||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Cvl|Construcao e reformas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Jcm Empreendimentos Imobiliarios|Construcao e reformas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Solucao Empreendimentos Imobiliarios|Construcao e reformas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Gc Construcoes|Construcao e reformas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Jardins da Praca Empreendimento Imobiliario|Construcao e reformas||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Cicera Cely Silva Brito|Pintor||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1562259949-e8e7689d7828?auto=format&fit=crop&w=500&q=80
+Cicero dos Santos Pereira|Pintor||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1562259949-e8e7689d7828?auto=format&fit=crop&w=500&q=80
+Leandro Leite da Silva|Pintor||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1562259949-e8e7689d7828?auto=format&fit=crop&w=500&q=80
+Elcio dos Santos|Pintor||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1562259949-e8e7689d7828?auto=format&fit=crop&w=500&q=80
+Otto Magdiel Fotografia|Fotografo e filmagem|(88) 99642-0846|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1516035069371-29a1b244cc32?auto=format&fit=crop&w=500&q=80
+Anselmo Producoes Fotografia e Filmagem|Fotografo e filmagem|(88) 3512-5504|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1516035069371-29a1b244cc32?auto=format&fit=crop&w=500&q=80
+Julio Henrique Fotografia|Fotografo e filmagem|(88) 99801-0770|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1516035069371-29a1b244cc32?auto=format&fit=crop&w=500&q=80
+Barbearia DL|Beleza e cabelo|(88) 98853-3908|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=500&q=80
+Dom Barbeiro|Beleza e cabelo|(88) 99634-9670|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=500&q=80
+FEMINE Studio de Beleza|Beleza e cabelo|(88) 98169-9151|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=500&q=80
+Studio Tamires Silva|Beleza e cabelo||Juazeiro do Norte - CE|https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=500&q=80
+Team Fast|Beleza e cabelo|(88) 99986-2436|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1560066984-138dadb4c035?auto=format&fit=crop&w=500&q=80
+Rede Tecnologia|Assistencia tecnica de informatica|(88) 98877-6717|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1593642532400-2682810df593?auto=format&fit=crop&w=500&q=80
+JF Manutencao Informatica|Assistencia tecnica de informatica|(88) 98193-4496|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1593642532400-2682810df593?auto=format&fit=crop&w=500&q=80
+Eres Informatica|Assistencia tecnica de informatica|(88) 3572-3350|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1593642532400-2682810df593?auto=format&fit=crop&w=500&q=80
+Chip7 Informatica|Assistencia tecnica de informatica|(88) 98823-4160|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1593642532400-2682810df593?auto=format&fit=crop&w=500&q=80
+MARMORARIA ANJO GABRIEL|Marmoraria|(88) 99669-8280|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1600607687920-4e2a09cf159d?auto=format&fit=crop&w=500&q=80
+Solaresp Energia Solar|Energia solar|(88) 99609-5799|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&w=500&q=80
+Sonnenschein Brasil|Servicos profissionais|(88) 2157-1314|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Clima Solar Cariri|Energia solar|(88) 99377-1670|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&w=500&q=80
+ESN Energia Solar|Energia solar|(88) 99977-8967|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&w=500&q=80
+TEC Segue Solar|Energia solar|(88) 98881-7572|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&w=500&q=80
+Tritium Energia Solar no Cariri|Energia solar|(88) 99901-0044|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&w=500&q=80
+AlexSat Antenista|Antenista|(88) 99903-0678|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1558618666-fcd25c85cd64?auto=format&fit=crop&w=500&q=80
+Conserta Smart Juazeiro do Norte|Servicos profissionais|(88) 99326-2626|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Anderson Chaveiro Assistencia 24hs|Chaveiro|(88) 98837-2012|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1582139329536-e7284fece509?auto=format&fit=crop&w=500&q=80
+Desentupidora Juazeiro - Desenjua|Desentupidora|(88) 98122-3283|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+ReA Limpeza e Higienizacao de Estofados|Limpeza e higienizacao|(88) 99660-6747|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1581578731548-c64695cc6952?auto=format&fit=crop&w=500&q=80
+Casa do Eletricista Home Center|Eletrica e automotiva|(88) 3102-4000|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1621905252507-b35492cc74b4?auto=format&fit=crop&w=500&q=80
+Busque & Ache Materiais|Servicos profissionais|(85) 9620-3897|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Boa Escolha Construcao|Construcao e reformas|(88) 8819-0565|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Casa do Ferro|Servicos profissionais|(83) 8134-4203|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Portal da Construcao|Construcao e reformas|(88) 3531-1326|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+V T Construcoes|Construcao e reformas|(88) 9973-0684|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+J.t. Pinheiro Cimentos|Servicos profissionais|(88) 3571-6948|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+F T Distribuidora de Cimento|Servicos profissionais|(88) 3571-6948|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80
+Rb Construcoes|Construcao e reformas|(88) 3571-8277|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1541888946425-d81bb19240f5?auto=format&fit=crop&w=500&q=80
+Gesso Gomes de Matos|Gesso e drywall|(88) 9990-4473|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1503387762-592deb58ef4e?auto=format&fit=crop&w=500&q=80
+Casa das Ferramentas|Servicos profissionais|(88) 3581-1586|Juazeiro do Norte - CE|https://images.unsplash.com/photo-1521737711867-e3b97375f902?auto=format&fit=crop&w=500&q=80`.split('\n').filter(Boolean).map(line=>{const [name,type,phone,address,image]=line.split('|');return {name,type,phone,address,city:'Juazeiro do Norte',image};});
+(function(){
+const cards=document.getElementById('cards'),serviceInput=document.getElementById('serviceInput'),cityInput=document.getElementById('cityInput');if(!cards)return;
+const norm=s=>(s||'').normalize('NFD').replace(/[\u0300-\u036f]/g,'').toLowerCase();
+const wa=(p,n)=>`https://wa.me/55${(p||'').replace(/\D/g,'')}?text=${encodeURIComponent(`Ola ${n}, encontrei seu perfil no TRAMPO e gostaria de saber mais sobre seu servico.`)}`;
+let box=document.getElementById('extraCards');if(!box){box=document.createElement('div');box.id='extraCards';box.className='cards';cards.insertAdjacentElement('afterend',box);}
+function render(){const s=norm(serviceInput?.value),c=norm(cityInput?.value);const list=extraProfessionals.filter(p=>{const h=norm(`${p.name} ${p.type} ${p.city} ${p.address}`);return(!s||h.includes(s))&&(!c||h.includes(c));});box.innerHTML=list.map(p=>`<article class="card"><div class="card-top"><img class="profile-photo" src="${p.image}" alt="Foto de ${p.name}" loading="lazy"></div><h3>${p.name}</h3><p class="type">${p.type}</p><p class="city">${p.city}</p><p class="city">${p.address}</p><p class="city">${p.phone}</p><div class="card-actions">${p.phone?`<a class="whatsapp-button" href="${wa(p.phone,p.name)}" target="_blank" rel="noopener noreferrer">Chamar no WhatsApp</a><a class="card-link" href="tel:${p.phone.replace(/\D/g,'')}">Ligar</a>`:''}</div></article>`).join('');}
+render();serviceInput?.addEventListener('input',render);cityInput?.addEventListener('input',render);document.querySelectorAll('#categories button').forEach(b=>b.addEventListener('click',()=>setTimeout(render,40)));document.getElementById('searchForm')?.addEventListener('submit',()=>setTimeout(render,40));
+})();
